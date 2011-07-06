4fc78349fc30fb0e3f1fdaedc205ce44d2fc6a57
Pere Martir <pere.martir4@gmail.com>
2011-07-06 13:50:34 +0200

    use soft's mustache to generate the table of all words

--- website/lists/index.js
+++ website/lists/index.js
@@ -1,22 +1,26 @@
 function(head, req) {
   var ddoc = this;
-  var Mustache = require("vendor/couchapp/lib/mustache");
+  // This code only works with a modified version of mustache from Sofa.  
+  var Mustache = require("lib/mustache");
   var List = require("vendor/couchapp/lib/list");
   var path = require("vendor/couchapp/lib/path").init(req);
   
   var indexPath = path.list('index', 'find', {});
   
   provides("html", function() {
+    var total_entries = 0;
     var stash = {
-      words : List.withRows(function(row) {
+      entries : List.withRows(function(row) {
+        var e = row.value;
         return {
-            word: row.value.word,
-            lang: row.value.lang,
-            lastOccurrences: row.value.timestamps[row.value.timestamps.length - 1],
-            occurrences: row.value.timestamps.length };
+          word: e.word,
+          lang: e.lang,            
+          occurrences: e.timestamp.length,
+          lastOccurrences: e.timestamp[row.value.timestamp.length - 1]
+        }
       })
     };
-    return Mustache.to_html(ddoc.templates.index, stash, ddoc.templates.partials, List.send);
+    return Mustache.to_html(ddoc.templates.index, stash, null, List.send);
   });
 
 }
